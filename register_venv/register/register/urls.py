04209fe41284d13b92174f0042313c3be4ae45f4
seungjin <seungjin429@naver.com>
2020-03-20 16:17:52 +0900

    naming 변경

--- register_venv/register/register/urls.py
+++ register_venv/register/register/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from signup.views import home
+from user.views import home
 
 urlpatterns = [
     path('', home),
     path('admin/', admin.site.urls),
-    path('register/', include('signup.urls'))
+    path('user/', include('user.urls'))
 ]
